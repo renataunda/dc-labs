1460a66e4e5bb121d26763d8c77757b7152d053c
Katacoda Scenario <scenario@katacoda.com>
2021-01-26 03:03:03 +0000

    Solve test-lab
    Signed-off-by: Katacoda Scenario <scenario@katacoda.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+  name := ""
+  
+  //foreach, para hacer un for debes nombrar la variable _
+  for _, word := range os.Args[1:]{
+    name += word + " "
+  }
+  if len(os.Args) <= 1{
+    fmt.Println("Error. Debes poner algun nombre")
+  }else{
+    fmt.Println("Welcome to the jungle", name)
+  }
 }
