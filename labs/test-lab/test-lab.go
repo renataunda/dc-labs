e1e50e8c5b1b848da7782e8a78d0484dc54d8e07
Renata Aparicio <renataunda11@gmail.com>
2021-02-12 04:54:22 +0000

    Primer submission

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+        "fmt"
+        "os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+        name := ""
+        //foreach, para hacer un for debes ponerle nombre a la variable_
+        for _, word:=range os.Args[1:]{
+                name += word + " "
+        }
+        if len(os.Args) <= 1{
+                fmt.Println("ERROOOOOORRRRRR")
+        }else{
+                fmt.Println("Welcome to the jungle", name)
+        }
 }
